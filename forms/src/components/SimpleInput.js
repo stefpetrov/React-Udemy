f62b37b8412f8bc9ad68c7a308ffdfd2453a7b35
Stefan Petrov <stefspetrov@gmail.com>
2022-10-13 11:59:21 +0300

    add validation for email input field

--- forms/src/components/SimpleInput.js
+++ forms/src/components/SimpleInput.js
@@ -5,19 +5,23 @@ const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState('')
   const [enteredNameIsTouched, setEnteredNameiSTouched] = useState(false)
 
+  const [enteredEmail, setEnteredEmail] = useState('')
+  const [enteredEmailIsTouched, setEnteredEmailisTouched] = useState(false)
+
 
   const enteredNameIsValid = enteredName.trim() !== ''
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameIsTouched
   let formIsValid = false
 
-  //  if we have more input fields we have to combine all validation in useEffect and update
-  //  the generic state formIsValid
+  const enteredEmailIsValid = enteredEmail.trim() !== '' && enteredEmail.includes('@')
+  const emailInputIsInvalid = !enteredEmailIsValid && enteredEmailIsTouched
 
-  if (enteredNameIsValid) {
+
+  if (enteredNameIsValid && enteredEmailIsValid) {
     formIsValid = true
   }
 
-  const inputChangeHandler = (event) => {
+  const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value)
   }
 
@@ -25,26 +29,47 @@ const SimpleInput = (props) => {
     setEnteredNameiSTouched(true)
   }
 
+  const emailInputChangeHandler = (event) => {
+    setEnteredEmail(event.target.value)
+
+  }
+
+  const emailInputBlurHandler = () => {
+    setEnteredEmailisTouched(true)
+  }
+
   const onSubmitHandler = (event) => {
     event.preventDefault()
     setEnteredNameiSTouched(true)
+    setEnteredEmailisTouched(true)
 
-    if (!enteredNameIsValid) {
+    if (!enteredNameIsValid || !enteredEmailIsValid) {
       return
     }
 
     setEnteredName('')
     setEnteredNameiSTouched(false)
+
+    setEnteredEmail('')
+    setEnteredEmailisTouched(false)
   }
 
   const nameInputClasses = nameInputIsInvalid ? 'form-control invalid' : 'form-control'
+  const emailInputClasses = emailInputIsInvalid ? 'form-control invalid' : 'form-control'
+
 
   return (
     <form onSubmit={onSubmitHandler}>
       <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input onBlur={nameInputBlurHandler} onChange={inputChangeHandler} type='text' id='name' value={enteredName} />
+        <input onBlur={nameInputBlurHandler} onChange={nameInputChangeHandler} type='text' id='name' value={enteredName} />
         {nameInputIsInvalid && <p className="error-text" >Name must not be empty!</p>}
+      </div>
+      <div className={emailInputClasses}>
+        <label htmlFor='email'>Your Email</label>
+        <input onBlur={emailInputBlurHandler} onChange={emailInputChangeHandler} type='email' id='email' value={enteredEmail} />
+        {emailInputIsInvalid && <p className="error-text" >Please add a valid email!</p>}
+
       </div>
       <div className="form-actions">
         <button disabled={!formIsValid} >Submit</button>
