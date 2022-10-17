eb33752758f9d200e9321ff350e5beb4c2b675be
Stefan Petrov <stefspetrov@gmail.com>
2022-10-17 12:37:03 +0300

    create store with createStore

---
+++ redux-demo/src/store/index.js
@@ -0,0 +1,25 @@
+import { createStore } from 'redux'
+
+
+const counterReducer = (state = { counter: 0 }, action) => {
+    if (action.type === 'increment') {
+
+        return {
+            counter: counter + 1
+        }
+    }
+    if (action.type === 'decrement') {
+
+        return {
+            counter: counter - 1
+        }
+    }
+
+    return state
+}
+
+
+const store = createStore(counterReducer)
+
+
+export default store
