318514fc3c9caade368496726f95c399d8295ca5
Stefan Petrov <stefspetrov@gmail.com>
2022-10-17 16:52:59 +0300

    use createSlice and configureStore

--- Redux-Toolkit-demo/src/store/index.js
+++ Redux-Toolkit-demo/src/store/index.js
@@ -1,52 +1,33 @@
-import { createStore } from 'redux'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
 
 const initialState = {
-    counter:0,
-    showCounter:true
+    counter: 0,
+    showCounter: true
 }
 
-
-const counterReducer = (state = initialState, action) => {
-    if (action.type === 'increment') {
-
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter
-        }
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: initialState,
+    reducers: {
+        increment(state) {
+            state.counter++
+        },
+        decrement(state) {
+            state.counter--
+        },
+        increase(state, action) {
+            state.counter += action.payload
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter
+        },
     }
+})
 
-    if (action.type === 'decrement') {
-
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter
-
-        }
-    }
-
-    if (action.type === 'increase') {
-
-        return {
-            counter: state.counter + action.payload,
-            showCounter: state.showCounter
-
-        }
-    }
-
-    if (action.type === "toggle") {
-
-        
-            return {
-                counter: state.counter,
-                showCounter: !state.showCounter
-            }
-            
-    }
-
-    return state
-}
-
-
-const store = createStore(counterReducer)
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
+// 'reducer' can be a object with many reducers, if we have
 
 export default store
