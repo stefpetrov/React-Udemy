a4936176e9980198104cdab23cc1118075c5d2e3
Stefan Petrov <stefspetrov@gmail.com>
2022-10-19 12:33:10 +0300

    use HTTP requests with Redux

--- redux-practice/src/App.js
+++ redux-practice/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -6,6 +7,18 @@ import Products from './components/Shop/Products';
 function App() {
 
   const showCart = useSelector(state => state.ui.cartIsVisible)
+  const cart = useSelector(state => state.cart)
+
+  useEffect(() => {
+    fetch('https://react-practice-a92c2-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+      {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(cart)
+      })
+  }, [cart])
 
 
   return (
