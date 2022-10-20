5fa16f7c69a4ad5758f6e80097c503c305198d2c
Stefan Petrov <stefspetrov@gmail.com>
2022-10-20 15:19:52 +0300

    add quote detail  page

--- Practice-Router-v5/src/components/quotes/QuoteItem.js
+++ Practice-Router-v5/src/components/quotes/QuoteItem.js
@@ -1,6 +1,10 @@
+import { Link } from 'react-router-dom';
 import classes from './QuoteItem.module.css';
 
 const QuoteItem = (props) => {
+
+
+
   return (
     <li className={classes.item}>
       <figure>
@@ -9,9 +13,9 @@ const QuoteItem = (props) => {
         </blockquote>
         <figcaption>{props.author}</figcaption>
       </figure>
-      <a className='btn'>
+      <Link to={`/quotes/${props.id}`} className='btn'>
         View Fullscreen
-      </a>
+      </Link>
     </li>
   );
 };
--- Practice-Router-v5/src/pages/QuouteDetail.js
+++ Practice-Router-v5/src/pages/QuouteDetail.js
@@ -1,13 +1,27 @@
 import { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import NoQuotesFound from "../components/quotes/NoQuotesFound";
+
+
+const DUMMY_QUOTES = [
+    { id: 'q1', author: 'Stefan', text: 'React is the best!' },
+    { id: 'q2', author: 'Stefan Petrov', text: 'Learning React is the great!' }
+]
 
 const QuouteDetail = () => {
     const params = useParams();
+
+    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+
+    if (!quote) {
+        return <p>No quotes found!</p>
+    }
+
     return (
-        <Fragment>  
-            <h1>Detailed Page</h1>
-            <p>{params.quoteId}</p>
+        <Fragment>
+            <HighlightedQuote quote={quote.author} text={quote.text} />
             <Route path={`/quotes/${params.quoteId}/comments`}>
                 <Comments />
             </Route>
