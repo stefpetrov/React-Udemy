6479c19b0bfccc6722966ea10e96865305d3bc65
Stefan Petrov <stefspetrov@gmail.com>
2022-10-20 13:43:42 +0300

    create Routes

--- Practice-Router-v5/src/App.js
+++ Practice-Router-v5/src/App.js
@@ -1,8 +1,21 @@
+import { Route, Switch } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuouteDetail from "./pages/QuouteDetail";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path='/quotes' exact>
+        <AllQuotes />
+      </Route>
+      <Route path='/quotes/:quoteId'>
+        <QuouteDetail />
+      </Route>
+      <Route path='/new-quote'>
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
---
+++ Practice-Router-v5/src/pages/QuouteDetail.js
@@ -0,0 +1,7 @@
+const QuouteDetail = () => {
+
+    return <h1>Quote Detail Page</h1>
+
+}
+
+export default QuouteDetail
