ddb857240ea08a681b1d3f361fa119112a3bfe99
Stefan Petrov <stefspetrov@gmail.com>
2022-10-24 17:57:08 +0300

    use Outlet

--- react-router-v6-update-second-example/src/pages/Products.js
+++ react-router-v6-update-second-example/src/pages/Products.js
@@ -1,6 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Products = () => {
+
+  const navigate = useNavigate()
+  navigate('/welcome')
+  navigate(-1)
+
+
   return (
     <section>
       <h1>The Products Page</h1>
--- react-router-v6-update-second-example/src/pages/Welcome.js
+++ react-router-v6-update-second-example/src/pages/Welcome.js
@@ -1,12 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Outlet, Route, Routes } from 'react-router-dom';
 
 const Welcome = () => {
   return (
     <section>
       <h1>The Welcome Page</h1>
-      <Routes>
-        <Route path="new-user" element={<p>Welcome, new user!</p>} />
-      </Routes>
+      <Link to='new-user'>New User</Link>
+      <Outlet />
+
     </section>
   );
 };
