8018992912b404dc2752090e0787034e19ff3415
Stefan Petrov <stefspetrov@gmail.com>
2022-10-27 19:06:58 +0300

    add auth data persisting

--- Authentication/src/store/auth-context.js
+++ Authentication/src/store/auth-context.js
@@ -5,22 +5,25 @@ import React, { useState } from "react"
 const AuthContext = React.createContext({
     token: '',
     isLoggedIn: false,
-    login: (token) => {},
-    logout: () => {}
+    login: (token) => { },
+    logout: () => { }
 })
 
 export const AuthContextProvider = (props) => {
-    const [token, setToken] = useState(null)
+    const initialToken = localStorage.getItem('token')
+    const [token, setToken] = useState(initialToken)
 
     const userIsLoggedIn = !!token
     //  that convert the value to truthy or falsy
 
     const loginHandler = (token) => {
         setToken(token)
+        localStorage.setItem('token', token)
     }
 
     const logoutHandler = () => {
         setToken(null)
+        localStorage.removeItem('token')
     }
 
     const contextValue = {
