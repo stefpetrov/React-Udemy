9d338a4b81a72f1681e3953e9d733a59e0686813
Stefan Petrov <stefspetrov@gmail.com>
2022-10-31 14:22:45 +0200

    add detail page

--- Next.js-practice/pages/[meetupId]/index.js
+++ Next.js-practice/pages/[meetupId]/index.js
@@ -1,1 +1,17 @@
 //domain.com/sometext
+
+import MeetupDetail from "../../components/meetups/MeetupDetail"
+
+const MeetUpDetails = () => {
+
+
+    <MeetupDetail
+        image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1598px-Stadtbild_M%C3%BCnchen.jpg" alt="A First Meetup'
+        title='A First Meetup'
+        address='Some Street 5, Some City'
+        description='The Meetup description' />
+   
+
+}
+
+export default MeetUpDetails
