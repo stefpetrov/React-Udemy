a9baa1c2b82b3d202971cbe3dae9f782d18cdc25
Stefan Petrov <stefspetrov@gmail.com>
2022-10-31 15:27:21 +0200

    use  getServerSideProps() to fetch data

--- Next.js-practice/pages/index.js
+++ Next.js-practice/pages/index.js
@@ -26,14 +26,31 @@ const HomePage = (props) => {
     return <MeetupList meetups={props.meetups} />
 }
 
+// you can run server side code here
+// fetch data from API
+// the page is pre-generated for every incoming request
+
+// export async function getServerSideProps(context) {
+//     const req = context.req
+//     const res = context.res
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUPS
+
+//         }
+//     }
+// }
+
 export async function getStaticProps() {
+    // you can run server side code here
     // we fetching data from API here insted of doing this in the component 
     // now data is not fetched on the second rerendered cycle, but during the build process
     //  GREAT FOR SEO, one of the main feature in NextJS(Data fetching for prerendering!)
     return {
         props: {
             meetups: DUMMY_MEETUPS
-        }
+        },
+        revalidate: 1
     }
 }
 
