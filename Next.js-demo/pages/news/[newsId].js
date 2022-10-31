179e8ace932f888e77fba4d7938812ea0b8bc8db
Stefan Petrov <stefspetrov@gmail.com>
2022-10-31 12:17:37 +0200

    add paths and dynamic paths

---
+++ Next.js-demo/pages/news/[newsId].js
@@ -0,0 +1,8 @@
+// domain.com/news/something-important
+const DetailPage = () => {
+
+    return <h1>The Detail Page</h1>
+
+}
+
+export default DetailPage
---
+++ Next.js-demo/pages/news/index.js
@@ -0,0 +1,9 @@
+// domain.com/news
+
+const NewsPage = () => {
+
+    return <h1>The News Page</h1>
+
+}
+
+export default NewsPage
