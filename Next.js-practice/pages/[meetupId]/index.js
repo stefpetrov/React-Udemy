9e7f59c5760c582beea571c5696db36839eeef9e
Stefan Petrov <stefspetrov@gmail.com>
2022-10-31 16:07:47 +0200

    introduce getStaticPaths()

--- Next.js-practice/pages/[meetupId]/index.js
+++ Next.js-practice/pages/[meetupId]/index.js
@@ -5,12 +5,13 @@ import MeetupDetail from "../../components/meetups/MeetupDetail"
 
 const MeetUpDetails = () => {
 
-
-    <MeetupDetail
-        image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1598px-Stadtbild_M%C3%BCnchen.jpg" alt="A First Meetup'
-        title='A First Meetup'
-        address='Some Street 5, Some City'
-        description='The Meetup description' />
+    return (
+        <MeetupDetail
+            image='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1598px-Stadtbild_M%C3%BCnchen.jpg' alt='A First Meetup'
+            title='A First Meetup'
+            address='Some Street 5, Some City'
+            description='The Meetup description' />
+    )
 }
 
 export async function getStaticPaths() {
