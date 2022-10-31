0ce9d2f4323677fe73bbad0ecfc19faedf75fbbf
Stefan Petrov <stefspetrov@gmail.com>
2022-10-31 13:15:00 +0200

    add a list of meetups

--- Next.js-practice/pages/index.js
+++ Next.js-practice/pages/index.js
@@ -1,1 +1,26 @@
 // domain.com/
+import MeetupList from "../components/meetups/MeetupList"
+
+const DUMMY_MEETUPS = [
+    {
+        id: 'm1',
+        title: 'A First Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1598px-Stadtbild_M%C3%BCnchen.jpg',
+        adress: 'Some Adress  6, 12345, Some City',
+        description: 'This is the first meetup!'
+    },
+    {
+        id: 'm2',
+        title: 'A Second Meetup',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1598px-Stadtbild_M%C3%BCnchen.jpg',
+        adress: 'Some Adress  4, 12345, Some City',
+        description: 'This is the second meetup!'
+    }
+]
+
+const HomePage = () => {
+
+    return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export default HomePage
