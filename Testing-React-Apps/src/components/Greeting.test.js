71666eecaf59df48e85e148245cd867d34027b96
Stefan Petrov <stefspetrov@gmail.com>
2022-11-09 15:35:12 +0200

    create suite and first tests

---
+++ Testing-React-Apps/src/components/Greeting.test.js
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react"
+import Greeting from "./Greeting"
+
+describe('Greeting component', () => {
+
+    test('render Hello World as a text', () => {
+        // Arrange
+        render(<Greeting />)
+
+        // Act
+        //... nothing
+
+        // Assert
+        const helloWorldElement = screen.getByText('Hello World', { exact: false })
+        expect(helloWorldElement).toBeInTheDocument()
+
+
+    })
+
+})
+
