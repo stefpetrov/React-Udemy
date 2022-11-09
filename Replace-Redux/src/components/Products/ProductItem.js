4cd008c3d35b07d72721366f52fdb1446d30ce6d
Stefan Petrov <stefspetrov@gmail.com>
2022-11-09 11:40:11 +0200

    replace Redux with React Context

--- Replace-Redux/src/components/Products/ProductItem.js
+++ Replace-Redux/src/components/Products/ProductItem.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 
 import Card from '../UI/Card';
 import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
+import { ProductContext } from '../../context/products-context';
 
 const ProductItem = props => {
 
+  const toggleFav = useContext(ProductContext).toggleFav
+
 
   const toggleFavHandler = () => {
-    // dispatch(toggleFav(props.id));
+    toggleFav(props.id)
   };
 
   return (
--- Replace-Redux/src/context/products-context.js
+++ Replace-Redux/src/context/products-context.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react"
 
 export const ProductContext = React.createContext({
-    products: []
+    products: [],
+    toggleFav: (id) => { }
 })
 
 export default (props) => {
@@ -32,8 +33,25 @@ export default (props) => {
             isFavorite: false
         }
     ])
+
+    const toggleFavorite = (productId) => {
+        setProductsList(currentProdList => {
+            const prodIndex = currentProdList.findIndex(
+                p => p.id === productId
+            );
+            const newFavStatus = !currentProdList[prodIndex].isFavorite;
+            const updatedProducts = [...currentProdList];
+            updatedProducts[prodIndex] = {
+                ...currentProdList[prodIndex],
+                isFavorite: newFavStatus
+            };
+
+            return updatedProducts
+        })
+
+    }
     return (
-        <ProductContext.Provider value={{ products: productsList }}>
+        <ProductContext.Provider value={{ products: productsList, toggleFav: toggleFavorite }}>
             {props.children}
         </ProductContext.Provider>
     )
