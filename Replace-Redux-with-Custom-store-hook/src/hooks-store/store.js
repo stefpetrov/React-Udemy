18e421f568f853c790fc921f654a43f40d2837dd
Stefan Petrov <stefspetrov@gmail.com>
2022-11-09 12:28:25 +0200

    create useStore Hook

---
+++ Replace-Redux-with-Custom-store-hook/src/hooks-store/store.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react"
+
+let globalState = {}
+let listeners = []
+let actions = {}
+
+const useStore = () => {
+    const setState = useState(globalState)[1]
+
+    useEffect(() => {
+        listeners.push(setState)
+
+        return () => {
+            listeners = listeners.filter(listener => listener !== setState)
+        }
+
+    }, [setState])
+
+
+}
