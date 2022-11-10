ceb41f4648d3af00e7570615df402810e75f1752
Stefan Petrov <stefspetrov@gmail.com>
2022-11-10 12:15:03 +0200

    use Mock fn insted of fetch

--- Testing-React-Apps/src/components/Async.test.js
+++ Testing-React-Apps/src/components/Async.test.js
@@ -5,6 +5,12 @@ import Async from "./Async"
 describe('Async component', () => {
 
     test('renders post if request succeeds', async () => {
+        // here we overwrite fetch function and use "mock" instead of generating a real request
+        window.fetch = jest.fn()
+        window.fetch.mockResolvedValueOnce({
+            json: async () => [{ id: 'p1', title: 'First post' }]
+        })
+
         render(<Async />)
 
         const listItemElements = await screen.findAllByRole('listitem')
