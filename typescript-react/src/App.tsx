7e36601ee593e01fe4d5dd811a3a017ef69996e5
Stefan Petrov <stefspetrov@gmail.com>
2022-11-11 09:14:42 +0200

    create Todos component

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import Todos from "./components/Todos";
 
 function App() {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
 }
 
 export default App;
