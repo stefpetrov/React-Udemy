3943d6876abb388e7f758a1566cd6d2c411ad811
Stefan Petrov <stefspetrov@gmail.com>
2022-11-11 15:23:36 +0200

    pass fn by props

--- typescript-react/src/App.tsx
+++ typescript-react/src/App.tsx
@@ -1,16 +1,24 @@
+import { useState } from "react";
 import "./App.css";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
 
-  const todos = [ 
-    new Todo('React'),
-    new Todo('TypeScript')
-  ]
+  const [todos, setTodos] = useState([])
+
+  const addTodoHandler = (todoText:string) => {
+
+    
+
+
+  }
+
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler}/>
       <Todos items={todos} />
     </div>
   );
--- typescript-react/src/components/NewTodo.tsx
+++ typescript-react/src/components/NewTodo.tsx
@@ -1,31 +1,33 @@
-import React, { RefObject, useRef } from "react";
+import React, { useRef } from "react";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
 
     const todoTextInputRef = useRef<HTMLInputElement>(null)
 
-  const submitHandler = (event:React.FormEvent) => {
-    event.preventDefault()
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault()
 
-    const enteredText = todoTextInputRef.current!.value 
-    //const enteredText = todoTextInputRef.current?.value we use ! instead of ? if we are sure, that value is not null
+        const enteredText = todoTextInputRef.current!.value
+        //const enteredText = todoTextInputRef.current?.value we use ! instead of ? if we are sure, that value is not null
 
-    if(enteredText.trim().length === 0){
-        // throw error
-        return
-    }
+        if (enteredText.trim().length === 0) {
+            // throw error
+            return
+        }
 
-    
+        props.onAddTodo(enteredText)
 
-  };
 
-  return (
-    <form onSubmit={submitHandler}>
-      <label htmlFor="text">Todo text</label>
-      <input type="text" id="text" ref={todoTextInputRef} />
-      <button>Add Todo</button>
-    </form>
-  );
+
+    };
+
+    return (
+        <form onSubmit={submitHandler}>
+            <label htmlFor="text">Todo text</label>
+            <input type="text" id="text" ref={todoTextInputRef} />
+            <button>Add Todo</button>
+        </form>
+    );
 };
 
 export default NewTodo;
